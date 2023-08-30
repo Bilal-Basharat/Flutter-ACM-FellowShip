f73d7ba8adc26c3ae344038023d032bbcb099f78
Bilal Basharat <bilalbisharat@gmail.com>
2023-08-30 11:45:39 +0500

    Week-5: Practised Future and Stream provider

--- Week_5/lib/main.dart
+++ Week_5/lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const ProviderScope(child:  MyApp()));
@@ -11,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,13 +19,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage2(),
+      home: ShowFutureProvider(),
     );
   }
 }
 
 final counterStateProvider = StateProvider<int>((ref) {
-      return 0;
+  return 0;
 });
 
 class MyHomePage extends ConsumerWidget {
@@ -35,36 +34,36 @@ class MyHomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var value = ref.watch(counterStateProvider);
     return Scaffold(
-        body: Center(
-          child: Text('Value is $value', style: Theme
-              .of(context)
-              .textTheme
-              .headline4,),
+      body: Center(
+        child: Text('Value is $value', style: Theme
+            .of(context)
+            .textTheme
+            .headline4,),
+      ),
+      floatingActionButton:
+      Padding(
+        padding: EdgeInsets.only(left: 30.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () =>
+              ref
+                  .read(counterStateProvider.state)
+                  .state++,
+              child: Icon(Icons.add),
+            ),
+            Expanded(child: Container()),
+            FloatingActionButton(
+              onPressed: () =>
+              ref
+                  .read(counterStateProvider.state)
+                  .state--,
+              child: Icon(Icons.remove),
+            ),
+          ],
         ),
-        floatingActionButton:
-        Padding(
-          padding: EdgeInsets.only(left: 30.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              FloatingActionButton(
-                onPressed: () =>
-                ref
-                    .read(counterStateProvider.state)
-                    .state++,
-                child: Icon(Icons.add),
-              ),
-              Expanded(child: Container()),
-              FloatingActionButton(
-                onPressed: () =>
-                ref
-                    .read(counterStateProvider.state)
-                    .state--,
-                child: Icon(Icons.remove),
-              ),
-            ],
-          ),
-        ),
+      ),
     );
   }
 }
@@ -89,19 +88,69 @@ final clockProvider = StateNotifierProvider<Clock, DateTime>((ref){
   return Clock();
 });
 
-
 class HomePage2 extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref){
     final currentTime = ref.watch(clockProvider);
     final timeFormatted = DateFormat.Hms().format(currentTime);
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Current Time: ${timeFormatted}',
-          style: Theme.of(context).textTheme.headline4,
-        ),
-      )
+        body: Center(
+          child: Text(
+            'Current Time: ${timeFormatted}',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        )
     );
   }
 }
+
+
+//fetch data through future provider
+Future<String> fetchWeatherReport() async {
+  return await Future.delayed(
+    const Duration(seconds: 5),(){
+      return '33\u2103';
+  }
+  );
+}
+
+final myFutureProvider = FutureProvider<String>((ref){
+  return fetchWeatherReport();
+});
+
+//showing data using stream provider
+final numbersProvider =  StreamProvider<int>((ref){
+  return fetchNumbers();
+},);
+
+Stream<int> fetchNumbers(){
+  return Stream<int>.periodic(
+      const Duration(seconds: 1), (number){
+    return number;
+  }
+  ).take(100);
+}
+
+class ShowFutureProvider extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref){
+    final futureValue = ref.watch(myFutureProvider);
+    final streamValue = ref.watch(numbersProvider);
+    return Scaffold(
+      body: Center(
+        child: streamValue.when(
+          data: (data) => Text('value is: $data',
+          style: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+          ),),
+          loading: () => CircularProgressIndicator(),
+          error: (e, st) => Text('error: $e'),
+        ),
+      ),
+    );
+  }
+}
+
