4ba95ff856ab0965bc2edc742e3d57837e53621b
Bilal Basharat <bilalbisharat@gmail.com>
2023-08-31 12:19:04 +0500

    UI of Dice game is completed

--- Flutter_Dice_Game/lib/main.dart
+++ Flutter_Dice_Game/lib/main.dart
@@ -18,6 +18,28 @@ class DiceApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+      child:TextButton(
+        onPressed: (){
+          print('Left Button is pressed');
+        },
+        child: Image.asset(
+            'images/dice1.png'),
+      ),
+      ),
+          Expanded(
+      child: TextButton(
+        onPressed: (){
+          print('Right Button is pressed');
+        },
+        child: Image.asset(
+        'images/dice1.png'),
+      ),),
+        ],
+      ),
+    );
   }
 }
