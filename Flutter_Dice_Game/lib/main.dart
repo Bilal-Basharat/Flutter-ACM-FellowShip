35eb00fbf27c716c63bea9f7e9ab289d29e64f19
Bilal Basharat <bilalbisharat@gmail.com>
2023-09-01 12:22:32 +0500

    dice game logic completed

--- Flutter_Dice_Game/lib/main.dart
+++ Flutter_Dice_Game/lib/main.dart
@@ -4,10 +4,11 @@ import 'dart:math';
 void main() {
   return runApp(
       MaterialApp(
-        home: Scaffold(p
+        home: Scaffold(
+          backgroundColor: Colors.lightBlueAccent,
           appBar: AppBar(
             title: Center(child: Text("Dice App")),
-            backgroundColor: Colors.deepOrange,
+            backgroundColor: Colors.blue.shade800,
           ),
           body: DiceApp(),
         ),
@@ -22,8 +23,9 @@ class DiceApp extends StatefulWidget {
 class _DiceAppState extends State<DiceApp> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  String result = "Click on any dice";
 
-  //function for generating random numbers for left and right dice  
+  //function for generating random numbers for left and right dice
   void changeDiceFace(){
     setState((){
       leftDiceNumber = Random().nextInt(5) + 1;
@@ -33,31 +35,83 @@ class _DiceAppState extends State<DiceApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          Expanded(
-            child:TextButton(
-              onPressed: (){
 
-              },
-              child: Image.asset(
-                  'images/dice$leftDiceNumber.png'),
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+    ),
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               Text("Click on Any Dice",
+            style:       TextStyle(
+              color: Colors.white,
+              fontSize: 25,
             ),
-          ),
+           ),
+          ],),
+
+        SizedBox(
+          height: 100,
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child:TextButton(
+                  onPressed: (){
+                    changeDiceFace();
+                  },
+                  child: Image.asset(
+                      'images/dice$leftDiceNumber.png'),
+                ),
+              ),
+
           Expanded(
             child: TextButton(
               onPressed: (){
-              setState((){
-              leftDiceNumber = Random().nextInt(5) + 1;
-              rightDiceNumber = Random().nextInt(5) + 1;
-              });
+                changeDiceFace();
     },
               child: Image.asset(
                   'images/dice$rightDiceNumber.png'),
             ),),
-        ],
-      ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 100,
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if(leftDiceNumber > rightDiceNumber)
+              Text("Player 1 Wins",
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 5.0,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,),)
+            else if(rightDiceNumber > leftDiceNumber)
+              Text("Player 2 Wins",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 5.0,
+                  fontSize: 36,),)
+            else
+              Text("Draw",
+                style:
+                TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 5.0,
+                  fontSize: 36,),)
+          ],),
+      ],
     );
   }
 }
