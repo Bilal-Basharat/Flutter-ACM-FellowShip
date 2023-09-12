7bceb42da97aa5ab3d4cae55f6d8f2a1b1448c33
Bilal Basharat <bilalbisharat@gmail.com>
2023-09-13 00:50:31 +0500

    Week-6: Flutter Image Upload To API Server Using Multiport Http Request with Null Safety

--- Week_6/lib/main.dart
+++ Week_6/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:api_integration/ComplexAPIExample.dart';
+import 'package:api_integration/Models/UploadImageScreen.dart';
 import 'package:api_integration/SignUp.dart';
 import 'Photos_example.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: SignUpScreen(),
+      home: UploadImageScreen(),
     );
   }
 }
