e718d29f8a8921a888818de12fd365247aee4b53
Bilal Basharat <bilalbisharat@gmail.com>
2023-09-23 21:22:09 +0500

    add constants class and some fixes

---
+++ Week_8/climate_forcast/lib/Models/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+  final Color backGroundColor = const Color(0xff5CD2E6);
+  final Color primaryColor = const Color(0xff3085C3);
+  final Color secondaryColor = const Color(0xff90B2F8);
+}
--- Week_8/climate_forcast/lib/screens/loading_screen.dart
+++ Week_8/climate_forcast/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:climate_forcast/models/constants.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -11,12 +12,15 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
+
+    Constants myConstants = Constants();
       Size size = MediaQuery.of(context).size;
-    return Scaffold(
+
+      return Scaffold(
       body: Container(
         width: size.width,
         height: size.height,
-        color: Colors.red,
+        color: myConstants.backGroundColor.withOpacity(1),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -24,16 +28,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
             children: [
               Image.asset('assets/get-started.png'),
               const SizedBox(height: 30),
-              Container(
-                width: size.width * 0.7,
-                height: 50,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                ),
-                child: const Center(
-                  child: Text('Get Started',
-                  style: TextStyle(color: Colors.white, fontSize: 18),),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context, 
+                  MaterialPageRoute(builder: (context) => const Welcome()));
+                },
+                child: Container(
+                  width: size.width * 0.7,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: myConstants.primaryColor,
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  child: const Center(
+                    child: Text('Get Started',
+                    style: TextStyle(color: Colors.white, fontSize: 18),),
+                  ),
                 ),
               ),
             ],
